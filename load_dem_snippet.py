17cc24ed607cfa9567a10708b56e394a79828063
Michael Christoffersen <mchristo28@gmail.com>
2025-04-04 11:25:54 -0400

    updated dem loading

--- load_dem_snippet.py
+++ load_dem_snippet.py
@@ -1,6 +1,10 @@
-v_dg = df.VectorFunctionSpace(self.model.mesh,self.model.E_thk)
-X = df.interpolate(self.model.mesh.coordinates,v_dg)
-meshx = X.dat.data_ro[:,0]*1000
-meshy = X.dat.data_ro[:,1]*1000
+import firedrake as df
+
+W = df.VectorFunctionSpace(mesh, family="CG", degree=2)
+x = df.interpolate(mesh.coordinates, W)
+meshx = x.dat.data_ro[:,0]
+meshy = x.dat.data_ro[:,1]
+
+b = df.function(Q)
 with rio.open("./data/frenchjoe_srtm_utm12n.tif") as src:
-            self.model.B.dat.data[:] = np.array([pnt[0] for pnt in src.sample(zip(meshx, meshy))])/1000
+            b.dat.data[:] = np.array([pnt[0] for pnt in src.sample(zip(meshx, meshy))])
